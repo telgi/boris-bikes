71e45793706f9c5474113d57ee90f1e93525d40c
Laura Chan <laurachan205@gmail.com>
2018-01-23 16:42:49 +0000

    Add release_bike method to DockingStation Class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,10 @@
 class DockingStation
 
+def release_bike
 end
+
+
+end
+
+dock = DockingStation.new
+dock.release_bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
 require 'docking_station'
 
-describe DockingStation do
-
+RSpec.describe DockingStation do
+  describe 'when called' do
+    it {is_expected.to respond_to(:release_bike)}
+  end
 end
