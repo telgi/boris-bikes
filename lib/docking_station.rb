a052ecb2234e4be56b88bae8260e0c1b2f6311e3
Terry Allitt <tallitt88@gmail.com>
2018-01-24 23:43:12 +0000

    Initialise bikes instance variable as array and change rest of code to reflect this

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,19 @@
 class DockingStation
 
-  attr_reader :bike
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def dock(bike)
-    fail "There are no more spaces left!" if @bike
-    @bike = bike
+    fail "There are no more spaces left!" if @bikes.count >= 20
+    @bikes << bike
   end
 
   def release_bike
-    fail "There are no bikes!" unless @bike
-    @bike
+    fail "There are no bikes!" if @bikes.empty?
+    @bikes.pop
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe DockingStation do
 
   bike = Bike.new
 
-  it {is_expected.to respond_to(:bike)}
+  it {is_expected.to respond_to(:bikes)}
 
   describe '#release_bike' do
 
@@ -26,11 +26,11 @@ RSpec.describe DockingStation do
     it {is_expected.to respond_to(:dock).with(1).argument}
 
     it 'docks a bike' do
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
 
-    it 'raises error when capacity has been reached' do
-      subject.dock(bike)
+    it 'raises error when the capacity of 20 bikes has been reached' do
+      20.times { subject.dock(bike) }
       expect {subject.dock(bike)}.to raise_error("There are no more spaces left!")
     end
 
