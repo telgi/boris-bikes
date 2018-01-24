209e4fa74101252ee4c8a5cb694b94f249f4091b
Terry Allitt <tallitt88@gmail.com>
2018-01-24 22:04:19 +0000

    Raise error on release_bike method if there are no available bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,11 +3,12 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    Bike.new
+    fail "There are no bikes!" unless @bike
+    @bike
   end
 
   def dock(bike)
-    bike
+    @bike = bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,21 +6,34 @@ RSpec.describe DockingStation do
 
   it {is_expected.to respond_to(:bike)}
 
-  it {is_expected.to respond_to(:release_bike)}
+  describe '#release_bike' do
 
-  it 'releases working bikes' do
-    expect(bike).to be_working
+    it {is_expected.to respond_to(:release_bike)}
+
+    it 'releases a bike' do
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises error when there are no bikes available' do
+      expect {subject.release_bike}.to raise_error("There are no bikes!")
+    end
+
   end
 
-  it {is_expected.to respond_to(:dock).with(1).argument}
+  describe '#dock' do
 
-  it 'docks a bike' do
-    expect(subject.dock(bike)).to eq bike
+    it {is_expected.to respond_to(:dock).with(1).argument}
+
+    it 'docks a bike' do
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'returns docked bikes' do
+      subject.dock(bike)
+      expect(subject.dock(bike)).to eq bike
+    end
+
   end
 
-  it 'returns docked bikes' do
-    subject.dock(bike)
-    expect(subject.dock(bike)).to eq bike
-  end
-  
 end
