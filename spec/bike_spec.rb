186f2139ac99f1cc4b409b19bfdb8cfd87e4abe7
Hannah Lillis <hannahamanda16@gmail.com>
2018-01-26 15:20:59 +0000

    Chapter 18 - complete first user story to report bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,17 @@
 require 'bike'
 
 RSpec.describe Bike do
+
   describe 'when called' do
-  it {is_expected.to respond_to(:working?)}
+    it {is_expected.to respond_to(:working?)}
   end
+
+  describe '#report' do
+    it {is_expected.to respond_to(:report)}
+
+    it 'returns a message when reported' do
+      expect(subject.report).to eq "Bike is broken"
+    end
+  end
+
 end
